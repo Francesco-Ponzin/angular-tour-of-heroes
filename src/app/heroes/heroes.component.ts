767157be4d03540480a966504bf0cd1bb8595025
Francesco Ponzin <francesco.ponzin@gmail.com>
2020-03-26 13:33:26 +0100

    service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,7 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
@@ -18,9 +19,14 @@ export class HeroesComponent implements OnInit {
   }
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
